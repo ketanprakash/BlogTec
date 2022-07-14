c74ead53a2094bd775258bdc943f1b4bd8b655fb
ketanprakash <ketanprakash001@gmail.com>
2022-07-14 20:59:52 +0530

    added middilewares for sites not found

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const logger = require('./middlewares/logger');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+app.use(logger);
+app.listen(port);
+
+app.get('*', (req, res) => {
+  res.status(404).send('<h1 style="color: red">404 Not Found</h1>')
 });
